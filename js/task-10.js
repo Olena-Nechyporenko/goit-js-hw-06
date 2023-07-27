5df2168d75b38bad7aa9f11dd408a8351c1b38fa
Olena-Nechyporenko <olenan0309@gmail.com>
2023-07-27 17:50:28 +0300

    fixed errors

--- js/task-10.js
+++ js/task-10.js
@@ -1,20 +1,20 @@
 
-const input = document.querySelector("input")
-const boxes = document.querySelector("#boxes")
-const btn = document.querySelectorAll("button")
+// const input = document.querySelector("input")
+// const boxes = document.querySelector("#boxes")
+// const btn = document.querySelectorAll("button")
 
 
 
-function createBoxes(amount) {
-const el = document.createElement("div")
-el.style.width = "30px"
-el.style.height = "30px"
-el.style.backgroundColor = getRandomHexColor()
-}
+// function createBoxes(amount) {
+// const el = document.createElement("div")
+// el.style.width = "30px"
+// el.style.height = "30px"
+// el.style.backgroundColor = getRandomHexColor()
+// }
 
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
